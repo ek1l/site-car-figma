b0ee5a39acd73fb81787549e64c7bd76933bd330
Leonardo Duarte <hintl3rgames2015@gmail.com>
2022-10-18 13:53:34 -0300

    terminei o site

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+
+ /* BOTAO RESPONSIVO MENU HAMBURGUER */
+
+ const btnMobile = document.getElementById('btnmobile')
+ function toggleMenu () {
+     const nav = document.getElementById('nav')
+     nav.classList.toggle('active')
+ }
+ btnMobile.addEventListener('click', toggleMenu)
